b4206f3074bdc790c3017e113d3e5d10c3585349
Radhe Saini <rsaini@vectorflow>
2023-05-08 14:22:07 +0530

    added new components for home page

---
+++ src/components/TopNav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './TopNav.css'
+
+export const TopNav = () => {
+  return (
+    <div className='topnav-main'>
+        <div className="left">
+            Radhe
+        </div>
+        <div className="right">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Projects</a>
+            <a href="">Experience</a>
+            <a href="">Contact</a>
+        </div>
+    </div>
+  )
+}
